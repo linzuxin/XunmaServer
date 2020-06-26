ff86b78b171a6b2ce26520f5f0bd63ba210a8288
sanjin <sanjin@163.com>
2020-06-26 18:24:28 +0400

    push

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-FROM ubuntu:latest
-ENV DEBIAN_FRONTEND=noninteractive
-ENV WORK="$HOME/grpc"
-ENV MYLOCALBIN="$HOME/mybin"
-ENV PATH="$PATH:$MYLOCALBIN/bin"
-WORKDIR $HOME
-RUN apt-get update \
+export DEBIAN_FRONTEND=noninteractive
+export WORK="$HOME/grpc"
+export MYLOCALBIN="$HOME/mybin"
+export PATH="$PATH:$MYLOCALBIN/bin"
+cd $HOME
+apt-get update \
     && apt-get install -y cmake build-essential autoconf libtool pkg-config git \
     && git clone --recurse-submodules -b v1.30.0 https://github.com/grpc/grpc \
     && mkdir -p "$MYLOCALBIN" \
